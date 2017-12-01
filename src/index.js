f7c03958a10933a136d49bd08fe2b561cded254c
Chetanpreet Malhi <chemalhi@gmail.com>
2017-12-01 17:59:35 -0500

    dev set up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './components/App';
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import allReducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(
+  allReducers,
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
